d24cd3a7fc653ebc1d1b04030c730c856f825ef8
R-chocolate <94609858+R-chocolate@users.noreply.github.com>
2025-11-24 23:57:45 +0800

    Refine responsive design for polygon and ball positioning
    Adjusted polygon radius and center position for better responsiveness on different screen sizes.

--- script.js
+++ script.js
@@ -34,11 +34,11 @@ function resize() {
         // [電腦版設定]
         // 半徑可以大一點，因為電腦螢幕寬
         // 注意：這裡改用 height * 0.35，確保上下不會頂到
-        polygonRadius = height * 0.30; 
+        polygonRadius = height * 0.28; 
         centerX = width / 2;
         // 電腦版中心點「不」上移，保持在正中間，或者只微調
         // 這樣就不會撞到上面的文字了
-        centerY = height / 2-10; 
+        centerY = height / 2-15; 
     }
     
     if (!isRunning) {
@@ -324,5 +324,6 @@ resetBtn.addEventListener('click', () => {
 });
 
 init();
+
 
 
